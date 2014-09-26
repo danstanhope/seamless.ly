c665833760f47b112531c99ba28faed14567bf0e
Dan Stanhope <danstanhope@gmail.com>
2014-09-26 14:45:12 -0400

    test changes and grunt setup

---
+++ test/test.js
@@ -0,0 +1,26 @@
+describe('SeamLess object exists', function () {
+    it('Ensure SeamLess object exists', function () {
+    	var sl = window.SeamLess;
+
+    	expect(sl).toBeDefined();
+    });
+});
+
+describe('SeamLess object creation', function () {
+    it('Ensure SeamLess can be created', function () {
+
+    	s = SeamLess.config({ window : window, origin : '*', frameId : 'test-frame' });
+
+    	expect(s).toBeDefined();
+    });
+});
+
+
+describe('SeamLess sendHeight', function () {
+    it('getHeight method', function () {
+
+    	s = SeamLess.config({ window : window, origin : '*', frameId : 'test-frame' });
+
+    	expect(s.getHeight()).toBeDefined();
+    });
+});
